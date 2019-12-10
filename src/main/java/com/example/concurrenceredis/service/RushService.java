8de8ad542e0e706aac197bf752fb9b9e990f86f8
xuexue95 <50290668+xuexue95@users.noreply.github.com>
2019-12-10 09:35:55 +0800

    create at 19.12.10
    springboot,reids

---
+++ src/main/java/com/example/concurrenceredis/service/RushService.java
@@ -0,0 +1,4 @@
+package com.example.concurrenceredis.service;
+
+public class RushService {
+}
---
+++ src/test/java/com/example/concurrenceredis/ConcurrenceRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.concurrenceredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrenceRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
